8fee3bd877c4688aa0ea356263594391bad4e531
robert-melena <99871728+robert-melena@users.noreply.github.com>
2026-03-05 17:28:48 -0800

    Added walls and changed camera position

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //reference to the game object
+    public GameObject seed;
+    //distance between camera and player
+    private Vector3 offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //to calc intial offset between the camera's position and he player position 
+        offset = transform.position - seed.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Maintain the same offset between the camera and player throughout the game.
+        transform.position = seed.transform.position;
+        
+    }
+}
