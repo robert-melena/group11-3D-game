2ccbc416e2760c0d459c3e8af297e3b9f575f580
TLellius <tonyxlau26x99@gmail.com>
2026-03-09 01:15:53 -0700

    camera, health, enemy collision changes
    Improved third person camera
    Health bar added
    Enemy collision added
    Player loses health when touching acid or enemy
    Player now gets knocked back from enemy collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ public class PlayerController : MonoBehaviour
 {
 
     public CharacterController controller;
+    public Transform cam;
     
+    float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
     public float speed = 5.0f;
-    public float rotateSpeed = 100.0f;
+    //public float rotateSpeed = 100.0f;
     public float jumpHeight = 1f;
 
     public float gravity = -9.81f;
@@ -16,45 +19,95 @@ public class PlayerController : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     public LayerMask acidMask;
+    public LayerMask enemyMask;
 
     Vector3 velocity;
-    bool isGrounded;
-    bool isOnAcid;
+    public bool isGrounded;
+    public bool isOnAcid;
+    public bool contactEnemy;
+    public float acidCooldown = 1f; //1 second
+    public float lastAcid = 0f;
+    private PlayerHealth playerHealth;
+
+    private Vector3 knockbackVelocity;
+    public float knockbackDuration = 0.5f;
+    private float knockbackTimer = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       playerHealth = GetComponent<PlayerHealth>();
+       if (playerHealth == null)
+       {
+            Debug.LogError("PlayerHealth not found on this GameObject!"); 
+       }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         isOnAcid = Physics.CheckSphere(groundCheck.position, groundDistance, acidMask);
 
-        if (isGrounded && velocity.y < 0)
+        if (knockbackTimer <= 0f)
         {
-            velocity.y = -2f;
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+            if (isOnAcid && Time.time - lastAcid >= acidCooldown)
+            {
+                playerHealth.TakeDamage(10f);
+                velocity.y = Mathf.Sqrt(jumpHeight * -5f * gravity);
+                lastAcid = Time.time;
+            }
+
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
         }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        else
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            velocity = knockbackVelocity;
+            knockbackTimer -= Time.deltaTime;
+            knockbackVelocity = Vector3.Lerp(knockbackVelocity, Vector3.zero, Time.deltaTime * 5f);
+            if (knockbackTimer <= 0f)
+            {
+                velocity -= knockbackVelocity;
+            }
         }
-        if (isOnAcid)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -4f * gravity);
-        }
-
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+    public void ApplyKnockback(Vector3 sourcePosition, float strength)
+    {
+        Vector3 dir = (transform.position - sourcePosition).normalized;
+        knockbackVelocity = dir * strength;
+        knockbackTimer = knockbackDuration;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Hit by enemy");
+            ApplyKnockback(other.transform.position, 40f);
+
+            playerHealth.TakeDamage(10f);
+        }
     }
 }
